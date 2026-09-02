3ab58694f0c8cc4a678571bccfd7189d0667e812
wmx2lihui <lihui2wmx@gmail.com>
2026-09-02 19:57:35 +0800

    add abstract generator skeleton

---
+++ src/generation/abstract_generator.py
@@ -0,0 +1,13 @@
+"""Abstract generation module.
+
+Generates structured scientific abstracts from research memory.
+"""
+
+class AbstractGenerator:
+    def generate(self, memory):
+        return {
+            "background": "",
+            "method": "",
+            "result": "",
+            "impact": "",
+        }
